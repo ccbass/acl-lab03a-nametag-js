0a1adbd397eca0b344ab4176c21ff41839806a7d
Colin Bassett <ccwbassett@gmail.com>
2021-02-10 22:03:55 -0800

    css button styling

--- nametag-js.js
+++ nametag-js.js
@@ -1,11 +1,6 @@
 let nametagName = document.getElementById("name") 
 let inputButton = document.getElementById("clickme")
 
-let pinkColorButton = document.getElementById("pink-color-btn")
-let greenColorButton = document.getElementById("green-color-btn")
-let blueColorButton = document.getElementById("blue-color-btn")
-
-
 const nameHandler = () => {
     const inputName = document.getElementById("input-name")
     let limit = ""
@@ -16,6 +11,11 @@ const nameHandler = () => {
     inputName.value = ""
 }
 
+
+let pinkColorButton = document.getElementById("pink-color-btn")
+let greenColorButton = document.getElementById("green-color-btn")
+let blueColorButton = document.getElementById("blue-color-btn")
+
 const colorHandler = (e) => {
     const tagTop = document.getElementById("nametag-top")
     const tagBottom = document.getElementById("nametag-bottom")
@@ -25,12 +25,9 @@ const colorHandler = (e) => {
 }
 
 
-
 inputButton.addEventListener("click", nameHandler)
 
 pinkColorButton.addEventListener("click", colorHandler)
 greenColorButton.addEventListener("click", colorHandler)
 blueColorButton.addEventListener("click", colorHandler)
 
-parent = document.querySelector("#button-modifiers").children
-console.log(parent)
